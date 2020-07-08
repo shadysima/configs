99e1ffc002bbd0d7941b69052ec282a76c6e4b07
nick <nick@localhost.localdomain>
2020-07-07 22:24:13 -0400

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,19 @@
+#        _               _           _                 _     
+#    ___| |__   __ _  __| |_   _ ___(_)_ __ ___   __ _( )___ 
+#   / __| '_ \ / _` |/ _` | | | / __| | '_ ` _ \ / _` |// __|
+#   \__ \ | | | (_| | (_| | |_| \__ \ | | | | | | (_| | \__ \
+#   |___/_| |_|\__,_|\__,_|\__, |___/_|_| |_| |_|\__,_| |___/
+#                          |___/                             
+#         __ _     _                        __ _       
+#        / _(_)___| |__     ___ ___  _ __  / _(_) __ _ 
+#       | |_| / __| '_ \   / __/ _ \| '_ \| |_| |/ _` |
+#       |  _| \__ \ | | | | (_| (_) | | | |  _| | (_| |
+#       |_| |_|___/_| |_|  \___\___/|_| |_|_| |_|\__, |
+#                                                |___/ 
+
+# AUTOSTART
+neofetch
+
 # USER ADDED ABBREVIATIONS
 abbr --add vfish "nvim ~/.config/fish/config.fish"
 abbr --add vbash "nvim ~/.bashrc"
@@ -26,3 +42,9 @@ set --export PATH $PATH /usr/local/go/bin
 # OTHER USER UPDATED ENV VARS
 set --export EDITOR nvim
 set --export BROWSER brave-browser
+set --export TERM alacritty
+
+
+# USER FUNCTIONS
+function fish_greeting
+end
